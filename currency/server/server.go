c1fc7725afc4dca714542114eb337368251b7baa
ellofae <sergei.bykovskiy2003@gmail.com>
2023-05-12 19:47:36 +0300

    Bi-directional streaming added v1.0.0

--- currency/server/server.go
+++ currency/server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"io"
+	"time"
 
 	"github.com/ellofae/gRPC-Bakery-Microservice/currency/data"
 	protos "github.com/ellofae/gRPC-Bakery-Microservice/currency/protos/currency"
@@ -26,4 +28,34 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 	}
 
 	return &protos.RateResponse{Rate: rate}, nil
+}
+
+func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
+
+	// blocking on server, but not on client
+	go func() {
+		for {
+			rr, err := src.Recv()
+			if err == io.EOF {
+				c.log.Info("Client has closed connection")
+				break
+			}
+
+			if err != nil {
+				c.log.Error("Unable to read from the client", "error", err)
+				break
+			}
+
+			c.log.Info("Handle client request", "request", rr)
+		}
+	}()
+
+	for {
+		err := src.Send(&protos.RateResponse{Rate: 12.1})
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
--- product-api/go.mod
+++ product-api/go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
+	github.com/Redocly/redoc v2.0.0+incompatible // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
