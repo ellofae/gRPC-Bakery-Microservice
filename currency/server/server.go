8856e108f619361de9fd6ffbb43a94961882cbdb
ellofae <sergei.bykovskiy2003@gmail.com>
2023-05-12 19:53:49 +0300

    Bi-directional streaming added v1.0.1 - docs added

--- currency/server/server.go
+++ currency/server/server.go
@@ -32,15 +32,17 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 
 func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
 
-	// blocking on server, but not on client
+	// handle client messages
 	go func() {
 		for {
 			rr, err := src.Recv()
+			// io.EOF signals that the client has closed the connection
 			if err == io.EOF {
 				c.log.Info("Client has closed connection")
 				break
 			}
 
+			// transport between client and server is unavailable
 			if err != nil {
 				c.log.Error("Unable to read from the client", "error", err)
 				break
@@ -50,7 +52,10 @@ func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) erro
 		}
 	}()
 
+	// handle server responses
+	// we block here to keep the connection open
 	for {
+		// send message back to the client
 		err := src.Send(&protos.RateResponse{Rate: 12.1})
 		if err != nil {
 			return err
